5876dde6cb7ce14bb3a87fc09bb1177ef4a05145
Tori Cruel <tarlan.quliev@icloud.com>
2023-05-29 17:25:34 +0300

    Initial Commit

---
+++ Clime/ClimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClimeApp.swift
+//  Clime
+//
+//  Created by Tarlan Quliev on 29.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ClimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
